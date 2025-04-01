a570789a76323e840a906d538e4dfce1056c328f
SHerlihy <steven_herlihy@yahoo.com>
2025-04-01 09:28:31 +0100

    update dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button, Input, Switch } from "@cqcl/quantinuum-ui";
 
-export default function App(){
-    return <div>
-        <Button variant="outline">Hello</Button>
-        <Input placeholder="say something"></Input>
-        <Switch />
-    </div>
+export default function App() {
+    return (
+        <main>
+            <Button variant="outline">Hello</Button>
+            <Input placeholder="say something"></Input>
+            <Switch />
+        </main>
+    )
 }
