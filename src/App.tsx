9adc3bba1ce283131ebbc10061122c6d2d3304e0
SHerlihy <steven_herlihy@yahoo.com>
2025-04-01 20:29:55 +0100

    display data from tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Button, Input, Switch } from "@cqcl/quantinuum-ui";
 import Layout from "./Layout";
+import { useQuery } from "@tanstack/react-query";
+import QueryKey from "./shared/queryKeys";
 
 export default function App() {
+    const { isPending, isError, data } = useQuery({
+        queryKey: [QueryKey.tags],
+        queryFn: () => ["tags"]
+    })
     return (
         <Layout>
-            <Button variant="outline">Hello</Button>
-            <Input placeholder="say something"></Input>
-            <Switch />
+            {isPending && <p>pending</p>}
+            {isError && <p>Error</p>}
+            {!isPending && !isError && <ul>
+                {data.map((tag) => <li key={tag}>{tag}</li>)}
+            </ul>
+            }
         </Layout>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import '@cqcl/quantinuum-ui/tokens.css'
 import './main.css'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>,
 )
