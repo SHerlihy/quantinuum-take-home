05db2b5c9ff6a0504ab60c0ec5d91307ed0d3851
SHerlihy <steven_herlihy@yahoo.com>
2025-04-02 12:09:29 +0100

    create tag search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import Layout from "./Layout";
-import { useQuery } from "@tanstack/react-query";
-import QueryKey from "./shared/queryKeys";
-import { Button, Command, CommandInput, CommandItem, CommandList } from "@cqcl/quantinuum-ui";
+import Layout from "./features/shared/Layout";
+import TagSearch from "./features/tagSearch/TagSearch";
 
 export default function App() {
-    const { isPending, isError, data } = useQuery({
-        queryKey: [QueryKey.tags],
-        queryFn: async () => {
-            const res = await fetch("https://cataas.com/api/tags")
-            if (!res.ok) {
-                throw new Error("Response not ok")
-            }
-
-            return res.json()
-        }
-    })
-
     return (
         <Layout>
-            {isPending && <p>pending</p>}
-            {isError && <p>Error</p>}
-            {!isPending && !isError && <Command>
-                <CommandInput placeholder="Search tags..." />
-                <CommandList>
-                    {data.map((tag) => <CommandItem key={`catTag-${tag}`} >
-                        <Button onClick={() => alert(tag)}>{tag}</Button>
-                    </CommandItem>
-                    )}
-                </CommandList>
-            </Command>
-            }
+            <TagSearch />
         </Layout>
     )
 }
