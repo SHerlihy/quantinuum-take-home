84b562c8f2a0c5bf3355c0cc7a6833047ed2aecf
SHerlihy <steven_herlihy@yahoo.com>
2025-04-02 22:53:48 +0100

    add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // Path to your src folder
-const root = path.resolve(__dirname, "src");export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": root, // Map @ to ./src
+const root = path.resolve(__dirname, "src");
+
+export default defineConfig({
+    plugins: [
+        TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), ,
+        react()
+    ],
+    resolve: {
+        alias: {
+            "@": root, // Map @ to ./src
+        },
     },
-  },
 });
