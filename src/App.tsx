cadde3934ff5f611ab6da24b575d710d489db5cb
SHerlihy <steven_herlihy@yahoo.com>
2025-04-02 23:39:32 +0100

    display search param data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Layout from "./features/shared/Layout";
 import TagSearch from "./features/tagSearch/TagSearch";
 import CatItem from "./features/displayCat/CatItem";
+import { Route } from "./routes";
 
 export default function App() {
+    const { tag } = Route.useSearch()
+
     return (
         <Layout>
+            <p>{tag}</p>
             <CatItem />
             <TagSearch />
         </Layout>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,15 @@
 import App from '@/App'
 import { createFileRoute } from '@tanstack/react-router'
 
+type AppSearch = {
+    tag: string
+}
+
 export const Route = createFileRoute('/')({
-  component: App,
+    component: App,
+    validateSearch: (search: Record<string, unknown>): AppSearch => {
+        return {
+            tag: (search.tag as string) || "",
+        }
+    }
 })
