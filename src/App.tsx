af14b6eae7eab8fa6bc0f96a9bc6b63f70ca5f01
SHerlihy <steven_herlihy@yahoo.com>
2025-04-02 10:57:56 +0100

    search tags in list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Layout from "./Layout";
 import { useQuery } from "@tanstack/react-query";
 import QueryKey from "./shared/queryKeys";
+import { Button, Command, CommandInput, CommandItem, CommandList } from "@cqcl/quantinuum-ui";
 
 export default function App() {
     const { isPending, isError, data } = useQuery({
@@ -19,9 +20,15 @@ export default function App() {
         <Layout>
             {isPending && <p>pending</p>}
             {isError && <p>Error</p>}
-            {!isPending && !isError && <ul>
-                {data.map((tag) => <li key={tag}>{tag}</li>)}
-            </ul>
+            {!isPending && !isError && <Command>
+                <CommandInput placeholder="Search tags..." />
+                <CommandList>
+                    {data.map((tag) => <CommandItem key={`catTag-${tag}`} >
+                        <Button onClick={() => alert(tag)}>{tag}</Button>
+                    </CommandItem>
+                    )}
+                </CommandList>
+            </Command>
             }
         </Layout>
     )
