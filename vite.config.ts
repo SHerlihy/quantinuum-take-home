349465b267879e4b001902b46ff13f169444f2f7
SHerlihy <steven_herlihy@yahoo.com>
2025-04-02 12:08:48 +0100

    add src path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// Path to your src folder
+const root = path.resolve(__dirname, "src");export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": root, // Map @ to ./src
+    },
+  },
+});
