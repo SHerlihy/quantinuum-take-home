38f4a02af91e2c86315f7153b4b7e36f9f7e910e
SHerlihy <steven_herlihy@yahoo.com>
2025-04-03 10:15:55 +0100

    change cats through pagination

--- src/features/catsViewer/index.tsx
+++ src/features/catsViewer/index.tsx
@@ -2,6 +2,8 @@ import QueryKey from '@/shared/queryKeys'
 import { useQuery } from '@tanstack/react-query'
 import CatItem from '../displayCat/CatItem'
 import { Route } from '@/routes'
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationPrevious } from '@cqcl/quantinuum-ui'
+import { Link } from '@tanstack/react-router'
 
 const CATS_PER_PAGE = 10
 
@@ -9,7 +11,7 @@ function CatsViewer() {
     const { tag, page } = Route.useSearch()
 
     const { isPending, isError, data } = useQuery({
-        queryKey: [QueryKey.cats + `${tag}`],
+        queryKey: [QueryKey.cats + `${tag}` +`${page}`],
         queryFn: async () => {
             const skip = `${(page - 1) * CATS_PER_PAGE}`
             const res = await fetch(`https://cataas.com/api/cats?tags=${tag}&skip=${skip}`)
@@ -41,6 +43,33 @@ md:grid-cols-5
                 {data.map(({ id }) => <CatItem key={id} id={id} />)}
             </div>
             }
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <Link
+                            from={Route.fullPath}
+                            search={(prev) => ({ ...prev, page: prev.page - 1 })}
+                        >prev</Link>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <Link
+                            from={Route.fullPath}
+                            search={(prev) => ({ ...prev, page: 1 })}
+                        />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <Link
+                            from={Route.fullPath}
+                            search={(prev) => ({ ...prev, page: prev.page + 1 })}
+                        >
+                            {">"}
+                        </Link>
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </section>
     )
 }
